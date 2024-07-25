6aa1dd75d50b675d696bdee1d3eed5e619fe2d14
kyle-berg <khb120@gmail.com>
2024-07-25 12:20:50 -0500

    bug still exists where it current movie doesnt update until second try

--- main.py
+++ main.py
@@ -35,12 +35,33 @@ def scan_site(driver):
     else:
         return 2
 
+def update_movies(driver, played_movies):
+    try:
+        WebDriverWait(driver, timeout=5).until(lambda success: success.find_elements(By.CLASS_NAME, "battle-movie"))
+    except:
+        print("Exception occurred finding the movies list.")
+        pass
+    else:
+        movie_element_list = driver.find_elements(By.CLASS_NAME, "battle-movie")
+        for movie in movie_element_list:
+            movie_str = movie.text
+            print(movie_str)
+            match = re.search(pattern=r"\d\n(.+?\))", string=movie_str, flags=re.MULTILINE)
+            if match:
+                movie_title = match.group(1)
+                match = re.search(r"(.+?)\((\d{4})\)", movie_title)
+                if not any(d['title'] == match.group(1).rstrip() for d in played_movies):
+                    movie = {'title': match.group(1).rstrip(), 'year': match.group(2)}
+                    played_movies.append(movie)
+            else:
+                update_movies(driver, played_movies)
+
 def play_movie(driver, played_movies):
-    curr_movie = played_movies[0]
+    curr_movie = played_movies[-1]
     print("Current movie: " + str(curr_movie["title"]) + " " + str(curr_movie["year"]))
 
     if len(played_movies) > 1:
-        prev_movie = played_movies[1]
+        prev_movie = played_movies[-2]
         print("Previous movie: " + str(prev_movie["title"]) + " " + str(prev_movie["year"]))
         links = api.gen_links(curr_movie, prev_movie)
     else:
@@ -81,29 +102,11 @@ def main():
 
     played_movies = []
     while True:
+        update_movies(driver, played_movies)
         game_status = scan_site(driver)
+        print("Played movies: " + str(played_movies))
         if game_status == 0: # My turn
             print("Your turn!")
-            time.sleep(1.0)
-            try:
-                WebDriverWait(driver, timeout=5).until(lambda success: success.find_elements(By.CLASS_NAME, "battle-movie"))
-            except:
-                print("Exception occurred finding the movies list.")
-                pass
-            else:
-                movie_element_list = driver.find_elements(By.CLASS_NAME, "battle-movie")
-                for movie in movie_element_list:
-                    movie_str = movie.text
-                    print(movie_str)
-                    match = re.search(pattern=r"\d\n(.+?\))", string=movie_str, flags=re.MULTILINE)
-                    if match:
-                        movie_title = match.group(1)
-                        match = re.search(r"(.+?)\((\d{4})\)", movie_title)
-                        if not any(d['title'] == match.group(1).rstrip() for d in played_movies):
-                            played_movies.append({'title': match.group(1).rstrip(), 'year': match.group(2)})
-                    else:
-                        print("No match found")
-            print("Played movies: " + str(played_movies))
             play_movie(driver, played_movies)
         elif game_status == 1: # Their turn
             print("Their turn!")
